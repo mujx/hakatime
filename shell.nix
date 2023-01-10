5be377143413a899ab124dcea64ff6687c9b373f
mujx <6430350+mujx@users.noreply.github.com>
2023-01-10 12:46:16 +0200

    Upgrade to GHC 9

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   name = "hakatime-dev";
 
   buildInputs = [
-    pkgs.haskell.compiler.ghc8107
+    pkgs.haskell.compiler.ghc924
     pkgs.cabal-install
     pkgs.zlib
     pkgs.postgresql
